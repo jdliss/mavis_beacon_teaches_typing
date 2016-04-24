119e22b42a575fef6fdc17517c778dda9d731436
Jonathan <jonacom@lissismore.com>
2016-04-24 11:21:54 -0600

    implement percentage correct method

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -1,4 +1,10 @@
 class Attempt < ActiveRecord::Base
   belongs_to :level
   validates :text, presence: true
+
+  def percent_correct
+    level.text.chars.map.with_index do |char, index|
+      char == text.chars[index] ? 1 : 0
+    end.reduce(:+)*100/level.text.length
+  end
 end
